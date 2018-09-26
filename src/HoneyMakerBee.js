2f5da15b7fe8d60387b9b906ff107d4bea350698
sveta0112 <sveta0112@hotmail.com>
2018-09-25 19:26:17 -0700

    finished inheritance practice with es6

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee{
+  constructor(color,food,eat){
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+    
+  }
+
+  makeHoney() {
+    this.honeyPot++;
+  }
+
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
